87eba2e23fed95a1577b359e6c426024723ff6ad
Anna Komar <anakomar586@gmail.com>
2025-05-10 17:44:53 +0300

    mycomm

---
+++ src/test/java/com/example/demo_online_shop/DemoOnlineShopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.demo_online_shop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoOnlineShopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
