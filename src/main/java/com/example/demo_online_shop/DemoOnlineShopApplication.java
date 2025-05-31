ac64ab54d0c8b82b71f48babc21e835b2f62de7c
annaandrusevych <annaandrusevic9@gmail.com>
2025-05-31 08:51:20 +0300

    Front something

--- src/main/java/com/example/demo_online_shop/DemoOnlineShopApplication.java
+++ src/main/java/com/example/demo_online_shop/DemoOnlineShopApplication.java
@@ -1,9 +1,15 @@
 package com.example.demo_online_shop;
 
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+
 
 @SpringBootApplication
+@EnableJpaRepositories("com.example.demo_online_shop.repository")
+@EntityScan(basePackages = "com.example.demo_online_shop.model")
 public class DemoOnlineShopApplication {
 
 	public static void main(String[] args) {
